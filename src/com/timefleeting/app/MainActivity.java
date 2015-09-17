89a75e29e3eccf1b845e5e2c20e1d30946f1bb6a
Weiping HUANG <Nightonke@outlook.com>
2015-09-17 21:57:24 +0800

    Add JazzyViewPager

--- src/com/timefleeting/app/MainActivity.java
+++ src/com/timefleeting/app/MainActivity.java
@@ -1,35 +1,107 @@
 package com.timefleeting.app;
 
+import java.util.List;
+
+import com.timefleeting.app.JazzyViewPager;
+import com.timefleeting.app.JazzyViewPager.TransitionEffect;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private JazzyViewPager mJazzy;
+	private LayoutInflater layoutInflater;	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		layoutInflater = getLayoutInflater().from(this);
+		setupJazziness(TransitionEffect.Tablet);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add("Toggle Fade");
+		String[] effects = this.getResources().getStringArray(R.array.jazzy_effects);
+		for (String effect : effects)
+			menu.add(effect);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getTitle().toString().equals("Toggle Fade")) {
+			mJazzy.setFadeEnabled(!mJazzy.getFadeEnabled());
+		} else {
+			TransitionEffect effect = TransitionEffect.valueOf(item.getTitle().toString());
+			setupJazziness(effect);
+		}
+		return true;
+	}
+
+	private void setupJazziness(TransitionEffect effect) {
+
+		mJazzy = (JazzyViewPager) findViewById(R.id.jazzy_pager);
+
+		mJazzy.setTransitionEffect(TransitionEffect.valueOf("CubeOut".toString()));
+		mJazzy.setAdapter(new MainAdapter());
+		mJazzy.setPageMargin(30);
+	}
+
+	private class MainAdapter extends PagerAdapter {
+		@Override
+		public Object instantiateItem(ViewGroup container, final int position) {
+			View view = new View(MainActivity.this);
+			View v;
+			if (position == 0) {
+				v = layoutInflater.inflate(R.layout.layout1, null);
+			} else if (position == 1) {
+				v = layoutInflater.inflate(R.layout.layout2, null);
+			} else if (position == 2) {
+				v = layoutInflater.inflate(R.layout.layout3, null);
+			} else {
+				v = layoutInflater.inflate(R.layout.layout4, null);
+			}
+			
+			mJazzy.setObjectForPosition(v, position);
+			container.addView(v, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+			
+			return v;
+		}
+		
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object obj) {
+			container.removeView(mJazzy.findViewFromObject(position));
+		}
+		
+		@Override
+		public int getCount() {
+			return 4;
+		}
+		
+		@Override
+		public boolean isViewFromObject(View view, Object obj) {
+			if (view instanceof OutlineContainer) {
+				return ((OutlineContainer) view).getChildAt(0) == obj;
+			} else {
+				return view == obj;
+			}
+		}		
+	}
 }
