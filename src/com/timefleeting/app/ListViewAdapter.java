a1207b4e48bf1c02e29af5bae58f607527b167b7
Weiping HUANG <Nightonke@outlook.com>
2015-09-22 17:55:40 +0800

    A more beautiful UI for the listview, and the Waves!

--- src/com/timefleeting/app/ListViewAdapter.java
+++ src/com/timefleeting/app/ListViewAdapter.java
@@ -1,5 +1,8 @@
 package com.timefleeting.app;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import com.daimajia.swipe.SimpleSwipeListener;
@@ -13,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,9 +24,12 @@ import android.widget.Toast;
 
 public class ListViewAdapter extends BaseSwipeAdapter {
 
+	private final int ALL_TIME = 7 * 1000 * 60 * 60 * 24;
+	
     private Context mContext;
     private List<Record> list;
     private int CurrentPosition;
+    
     SwipeLayout swipeLayout;
 
     public ListViewAdapter(List<Record> list, Context mContext) {
@@ -107,12 +114,7 @@ public class ListViewAdapter extends BaseSwipeAdapter {
                 Toast.makeText(mContext, "DoubleClick", Toast.LENGTH_SHORT).show();
             }
         });
-        v.findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(mContext, "click delete", Toast.LENGTH_SHORT).show();
-            }
-        });
+
         return v;
     }
 
@@ -123,9 +125,59 @@ public class ListViewAdapter extends BaseSwipeAdapter {
     	TextView titleTextView = (TextView)convertView.findViewById(R.id.listview_item_title);
     	titleTextView.setText(list.get(position).getTitle());
     	TextView remindTimeTextView = (TextView)convertView.findViewById(R.id.listview_item_remind_time);
-    	remindTimeTextView.setText(list.get(position).getRemindTime());
+    	remindTimeTextView.setText(list.get(position).getRemindTime().substring(5, 16));
     	TextView createTimeTextView = (TextView)convertView.findViewById(R.id.listview_item_create_time);
-    	createTimeTextView.setText(list.get(position).getCreateTime());
+    	createTimeTextView.setText(list.get(position).getCreateTime().substring(5, 16));
+    	String starString = list.get(position).getStar();
+    	ImageView star1 = (ImageView)convertView.findViewById(R.id.listview_item_star_1);
+    	ImageView star2 = (ImageView)convertView.findViewById(R.id.listview_item_star_2);
+    	ImageView star3 = (ImageView)convertView.findViewById(R.id.listview_item_star_3);
+    	ImageView star4 = (ImageView)convertView.findViewById(R.id.listview_item_star_4);
+    	ImageView star5 = (ImageView)convertView.findViewById(R.id.listview_item_star_5);
+    	if ("0".equals(starString)) {
+    		
+    	} else if ("1".equals(starString)) {
+    		star5.setImageResource(R.drawable.set_star);
+    	} else if ("2".equals(starString)) {
+    		star4.setImageResource(R.drawable.set_star);
+    		star5.setImageResource(R.drawable.set_star);
+    	} else if ("3".equals(starString)) {
+    		star3.setImageResource(R.drawable.set_star);
+    		star4.setImageResource(R.drawable.set_star);
+    		star5.setImageResource(R.drawable.set_star);
+    	} else if ("4".equals(starString)) {
+    		star2.setImageResource(R.drawable.set_star);
+    		star3.setImageResource(R.drawable.set_star);
+    		star4.setImageResource(R.drawable.set_star);
+    		star5.setImageResource(R.drawable.set_star);
+    	} else if ("5".equals(starString)) {
+    		star1.setImageResource(R.drawable.set_star);
+    		star2.setImageResource(R.drawable.set_star);
+    		star3.setImageResource(R.drawable.set_star);
+    		star4.setImageResource(R.drawable.set_star);
+    		star5.setImageResource(R.drawable.set_star);
+    	}
+    	
+    	WaveView waveView = (WaveView)convertView.findViewById(R.id.wave_view);
+    	
+    	SimpleDateFormat formatter = new SimpleDateFormat ("yyyy-MM-dd-HH:mm:ss");     
+		Date curDate = new Date(System.currentTimeMillis());
+		Date remindDate = new Date(System.currentTimeMillis());;
+		try {
+			remindDate = formatter.parse(list.get(position).getRemindTime());
+		} catch (ParseException p) {
+			p.printStackTrace();
+		}
+		long diff = remindDate.getTime() - curDate.getTime();
+		Log.d("TimeFleeting", diff + "");
+		if (diff < 0) {
+			diff = 0;
+		} else if (diff > ALL_TIME) {
+			diff = ALL_TIME;
+		}
+		diff = ALL_TIME - diff;
+		int progress = (int)(diff * 1.0 / ALL_TIME * 100) + 10;
+		waveView.setProgress(progress);
     }
 
     @Override
